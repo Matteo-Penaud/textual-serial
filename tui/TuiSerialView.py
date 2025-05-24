dc9476829dce2d1f70f7807d6f53828a81bb6a9d
Matteo-Penaud <72443905+Matteo-Penaud@users.noreply.github.com>
2025-05-24 09:42:30 +0200

    Added update text in the view for tests

--- tui/TuiSerialView.py
+++ tui/TuiSerialView.py
@@ -1,5 +1,13 @@
+from textual import work
 from textual.widgets import TextArea
 
 
 class TuiSerialView(TextArea):
     read_only = True
+
+    def on_mount(self) -> None:
+        self.update_timer = self.set_interval(1 / 100, self.update_serial)
+
+    @work(exclusive=True)
+    async def update_serial(self) -> None:
+        self.insert("Test")
